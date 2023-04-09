4a4900687a9106d8c88b237db2ab1df710b9b5a6
Jackson93150 <jackjack93150@gmail.com>
2023-04-09 20:49:21 +0200

    acount create

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const container = document.querySelector('.container');
 const tel = document.querySelector('#tel');
 const information = document.querySelector('.information');
 const form = document.querySelector('.form');
+const form_container = document.querySelector('.form-container');
 
 signupButton.addEventListener('click', function() {
   if(container.classList.contains('wide')) {
@@ -12,7 +13,8 @@ signupButton.addEventListener('click', function() {
     container.classList.add('deshrink');
     tel.classList.remove('wide');
     information.style.display = 'block';
-    form.style.display = 'none';
+    // form.style.display = 'none';
+    form_container.style.display = 'none';
   } else {
     signupButton.classList.add('wide');
     container.classList.add('wide');
@@ -20,7 +22,8 @@ signupButton.addEventListener('click', function() {
     container.classList.remove('deshrink');
     tel.classList.add('wide');
     information.style.display = 'none';
-    form.style.display = 'block';
+    form_container.style.display = 'flex';
+    // form.style.display = 'block';
   }
 });
 
