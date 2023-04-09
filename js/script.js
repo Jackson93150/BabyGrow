d7b89aeb60f9f504369aa6104f8490ce345d479f
Jackson93150 <jackjack93150@gmail.com>
2023-04-09 18:54:02 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const signupButton = document.querySelector('.signup');
+const container = document.querySelector('.container');
+const tel = document.querySelector('#tel');
+const information = document.querySelector('.information');
+const form = document.querySelector('.form');
+
+signupButton.addEventListener('click', function() {
+  if(container.classList.contains('wide')) {
+    signupButton.classList.remove('wide');
+    container.classList.remove('wide');
+    container.classList.remove('shrink');
+    container.classList.add('deshrink');
+    tel.classList.remove('wide');
+    information.style.display = 'block';
+    form.style.display = 'none';
+  } else {
+    signupButton.classList.add('wide');
+    container.classList.add('wide');
+    container.classList.add('shrink');
+    container.classList.remove('deshrink');
+    tel.classList.add('wide');
+    information.style.display = 'none';
+    form.style.display = 'block';
+  }
+});
+
